dfc8d0f4b37e7ed8f7206c4bd9a785c4eee96c31
Derek Hoagland <thegrickit@gmail.com>
2011-08-08 20:14:47 -0400

    Actually screw jquery. Reinventing wheels is good practice.

--- gambciv/async_jquery_include.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var jq = document.createElement('script');
-  jq.type = 'text/javascript';
-  jq.async = true;
-  jq.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(jq, s);
-})();
